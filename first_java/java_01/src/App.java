50d1d86d6d4e77758cb2402cb64714ae8a7cf1de
win-year <zhao14005791@outlook.com>
2024-12-13 13:46:31 +0800

    first commit

---
+++ first_java/java_01/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        int result = getSum(10, 20);
+        System.out.println("The sum is: " + result);
+    }
+
+    public static int getSum(int a, int b) {
+        return a + b;
+    }
+}
